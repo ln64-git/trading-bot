a7bc41ec8346f02001f3932d3f8595e12903714b
Luke <lucian.io@outlook.com>
2024-06-21 00:26:38 -0400

    layout

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -2,6 +2,8 @@ import type { Metadata } from "next";
 import { Inter } from "next/font/google";
 import "./globals.css";
 import ParticleBackground from "@/components/particle-background";
+import AgentCard from "@/components/agent-card";
+import StartCard from "@/components/start-program-card";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -20,13 +22,19 @@ export default function RootLayout({
       <body className={`${inter.className} relative`}>
         <ParticleBackground />
         <div className="absolute top-0 left-0 w-full h-screen bg-black bg-opacity-10 backdrop-blur-lg z-20"></div>
-        <div className="flex w-full h-screen absolute top-0 left-0 z-30">
-          <div className="flex-1 bg-red-950 h-full hidden sm:block">awd</div>
-          <div className="flex-1 bg-green-950 h-full sm:flex-1 sm:h-full sm:w-full">
-            awd
+        <div className="flex w-full h-screen absolute top-0 left-0 z-30 p-2">
+          <div className="bg-gray-700 bg-opacity-10 w-1/3 h-full rounded-tl-md rounded-bl-md hidden sm:flex flex-col justify-between">
+            <div className="h-full">
+              <AgentCard />
+            </div>
+            <div>
+              <StartCard />
+            </div>
+          </div>
+          <div className="flex-1 bg-gray-800 rounded-tr-md rounded-br-md bg-opacity-10 h-full sm:flex-1 sm:h-full sm:w-full">
+            {children}
           </div>
         </div>
-        {children}
       </body>
     </html>
   );
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,5 +1,3 @@
-import ParticleBackground from "@/components/particle-background";
-
 export default function Home() {
   return <div>Home</div>;
 }
